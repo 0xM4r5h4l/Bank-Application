d7789d3b0fe042bed40e03487fbce6fd32921243
admin <theblackerror19@gmail.com>
2025-03-18 07:12:22 +0200

    Small Fix

--- src/models/Admin.js
+++ src/models/Admin.js
@@ -63,13 +63,13 @@ AdminSchema.methods.comparePasswords = async function(reqPassword) {
     return await bcrypt.compare(reqPassword, this.password);
 };
 
-
 // TODO: Create updateAdmin method (requires: superadmin role) updateAdmin() {}
 
 
 AdminSchema.methods.createAdminJWT = async function(clientIp) {
-    this.lastLogin = Date.now;
+    this.lastLogin = new Date();
     this.lastLoginIp = clientIp || 'Unknown';
+    await this.save();
     return jwt.sign({
         userId: this._id,
         fullName: 'Unknown',
